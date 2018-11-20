3ee0effea81e3f24e82f448c9049bf9c95049d8b
liu.yalei <liu.yalei@puscene.com>
2018-11-20 20:02:26 +0800

    Hooks useEffect

--- src/routes/Hooks/Hooks.js
+++ src/routes/Hooks/Hooks.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from 'react';
 import {Card, Button } from 'antd';
 function Hooks(props) {
     // Declare a new state variable, which we'll call "count"
-    console.log(props);
+    // console.log(props);
     const [count, setCount] = useState(0)
 
     // Similar to componentDidMount and componentDidUpdate:
-
     useEffect(() => {
         // Update the document title using the browser API
         document.title = `You clicked ${count} times`;
