43ff235aed743553abe71e4481524b140bbddba4
liu.yalei <liu.yalei@puscene.com>
2018-11-20 14:26:48 +0800

    success use Hooks

--- src/routes/Hooks/Hooks.js
+++ src/routes/Hooks/Hooks.js
@@ -1,19 +1,20 @@
 import React, { Component, useState } from 'react';
-import PropTypes from 'prop-types';
-import { Table, Divider, Tag, Card, Button, Popconfirm, Input } from 'antd';
-import ImageWrapper from 'components/ImageWrapper'; // aware of the relative path
-//import TableForm from './TableForm';
+import {Card, Button } from 'antd';
 function Hooks() {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0)
     console.log(count);
     return (
-        <div>
-          <p>You clicked {count} times</p>
-          <button onClick={() => setCount(count + 1)}>
-            Click me
-          </button>
-        </div>
+          <div>
+            <Card title="React Hooks"  bordered={false}>
+              <div>
+                <p>You clicked {count} times</p>
+                <Button type="primary" onClick={() => setCount(count + 1)}>
+                  Click me
+                </Button>
+              </div>
+            </Card>
+          </div>
     );
 }
 export default Hooks;
