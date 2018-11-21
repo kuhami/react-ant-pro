cce9dc41e4e352165a90af24e3ea61b0112126ed
liu.yalei <liu.yalei@puscene.com>
2018-11-21 11:32:23 +0800

    Hooks

--- src/routes/Hooks/Hooks.js
+++ src/routes/Hooks/Hooks.js
@@ -4,21 +4,29 @@ function Hooks(props) {
     // Declare a new state variable, which we'll call "count"
     // console.log(props);
     const [count, setCount] = useState(0)
-
+    const [width, setWithd] = useState(window.innerWidth);
     // Similar to componentDidMount and componentDidUpdate:
     useEffect(() => {
         // Update the document title using the browser API
         document.title = `You clicked ${count} times`;
-    });
+        const handleResize = ()=>{
+            setWithd(window.innerWidth);
+        }
+        window.addEventListener('resize', handleResize);
+
+        // return () => (document.title = "前端精读");
+
+    },[width]);
 
     return (
           <div>
             <Card title="React Hooks"  bordered={false}>
               <div>
-                <p>You clicked {count} times</p>
-                <Button type="primary" onClick={() => setCount(count + 1)}>
-                  Click me
-                </Button>
+                    <p> 窗口宽度{ width }px</p>
+                    <p>You clicked {count} times</p>
+                    <Button type="primary" onClick={() => setCount(count + 1)}>
+                      Click me
+                    </Button>
               </div>
             </Card>
           </div>
