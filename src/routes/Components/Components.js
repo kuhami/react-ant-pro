1fbcb0d1fd5e2a2fd22a84251e413ec3e7625345
liu.yalei <liu.yalei@puscene.com>
2019-01-05 12:59:25 +0800

    tab 标签

--- src/routes/Components/Components.js
+++ src/routes/Components/Components.js
@@ -1,99 +1,18 @@
 import React,{Component} from 'react';
 import PropTypes from 'prop-types'
-import { Table,Card } from 'antd';
+import { Table,Card,Tabs, Button } from 'antd';
 import TreeCheck from 'components/TreeCheck'; // aware of the relative path
+const TabPane = Tabs.TabPane;
 
 export default class Components extends Component {
     constructor(props){
         super(props);
 
-    }
-    componentDidMount() {
-
-    }
-
-
-    onCheckedKeyChange = (checkedArrs)=>{
-        console.log(checkedArrs);
-    }
-
-    render() {
-
-        const treeDatas = [{
-            title: '0-0',
-            key: '0-0',
-            children: [{
-                title: '0-0-0',
-                key: '0-0-0',
-                children: [
-                    { title: '0-0-0-0', key: '0-0-0-0' },
-                    { title: '0-0-0-1', key: '0-0-0-1' },
-                    { title: '0-0-0-2', key: '0-0-0-2' },
-                ],
-            }, {
-                title: '0-0-1',
-                key: '0-0-1',
-                children: [
-                    { title: '0-0-1-0', key: '0-0-1-0' },
-                    { title: '0-0-1-1', key: '0-0-1-1' },
-                    { title: '0-0-1-2', key: '0-0-1-2' },
-                ],
-            }, {
-                title: '0-0-2',
-                key: '0-0-2',
-            }],
-        }, {
-            title: '0-1',
-            key: '0-1',
-            children: [
-                { title: '0-1-0-0', key: '0-1-0-0' },
-                { title: '0-1-0-1', key: '0-1-0-1' },
-                { title: '0-1-0-2', key: '0-1-0-2' },
-            ],
-        }, {
-            title: '0-2',
-            key: '0-2',
-        }];
-        const treeData = [{
-            label: '全部',
-            value: 'all',
-            children:[{
-                label: '0-0',
-                value: '00',
-                children: [{
-                    label: '0-0-0',
-                    value: '000',
-                    children: [
-                        { label: '0-0-0-0-0-0-0-0-0-0-0', value: '0000' },
-                        { label: '0-0-0-1', value: '0001' },
-                        { label: '0-0-0-2', value: '0002' },
-                    ],
-                }, {
-                    label: '0-0-1',
-                    value: '001',
-                    children: [
-                        { label: '0-0-1-0', value: '0010' },
-                        { label: '0-0-1-1', value: '0011' },
-                        { label: '0-0-1-2', value: '0012' },
-                    ],
-                }, {
-                    label: '0-0-2',
-                    value: '002',
-                }],
-            }, {
-                label: '0-1',
-                value: '01',
-                children: [
-                    { label: '0-1-0-0', value: '0100' },
-                    { label: '0-1-0-1', value: '0101' },
-                    { label: '0-1-0-2', value: '0102' },
-                ],
-            }, {
-                label: '0-2',
-                value: '02',
-            }]
-        }];
-
+        this.newTabIndex = 0;
+        const panes = [
+            { title: 'Tab 1', content: (<div >Content of Tab Pane 1</div>), key: '1' },
+            { title: 'Tab 2', content: 'Content of Tab Pane 2', key: '2' },
+        ];
         const columns = [{
             title: '参数	',
             dataIndex: 'Param',
@@ -115,8 +34,7 @@ export default class Components extends Component {
             key: 'Default',
             dataIndex: 'Default',
         }];
-
-        const data = [{
+        const dataSource = [{
             key: '1',
             Param: 'spanName', //参数
             explain: 'TreeCheck 选择器头部添加span标签', //说明
@@ -201,23 +119,214 @@ export default class Components extends Component {
             type: 'bool',
             Default: 'true',
         }];
+
+        this.state = {
+            activeKey: panes[0].key,
+            panes,
+            dataSource,
+            columns
+        };
+    }
+    componentDidMount() {
+
+    }
+
+    onChange = (activeKey) => {
+        this.setState({ activeKey });
+    }
+
+    onEdit = (targetKey, action) => {
+        this[action](targetKey);
+    }
+
+    add = () => {
+        const panes = this.state.panes;
+        const treeData = [{
+            label: '全部',
+            value: 'all',
+            children:[{
+                label: '0-0',
+                value: '00',
+                children: [{
+                    label: '0-0-0',
+                    value: '000',
+                    children: [
+                        { label: '0-0-0-0-0-0-0-0-0-0-0', value: '0000' },
+                        { label: '0-0-0-1', value: '0001' },
+                        { label: '0-0-0-2', value: '0002' },
+                    ],
+                }, {
+                    label: '0-0-1',
+                    value: '001',
+                    children: [
+                        { label: '0-0-1-0', value: '0010' },
+                        { label: '0-0-1-1', value: '0011' },
+                        { label: '0-0-1-2', value: '0012' },
+                    ],
+                }, {
+                    label: '0-0-2',
+                    value: '002',
+                }],
+            }, {
+                label: '0-1',
+                value: '01',
+                children: [
+                    { label: '0-1-0-0', value: '0100' },
+                    { label: '0-1-0-1', value: '0101' },
+                    { label: '0-1-0-2', value: '0102' },
+                ],
+            }, {
+                label: '0-2',
+                value: '02',
+            }]
+        }];
+        const activeKey = `newTab${this.newTabIndex++}`;
+        panes.push({ title: 'New Tab', content: (<Card title="多选SelectTree"  bordered={false}>
+            <TreeCheck
+                treeData={treeData}
+                isShowSearch={true}
+                spanName={'多选Select：'}
+                getAllNodes={true}
+                //LabelAndValue={['title','key']}
+                //checkedKeys={['00','01']}
+                multiple={true}
+                onCheckedKeyChange={(checkedArrs) => this.onCheckedKeyChange(checkedArrs)}/>
+            <h3>API</h3>
+            <p>TreeCheck 为多选SelectTree，本组件为适应特殊场景而封装。</p>
+            <Table columns={this.state.columns} dataSource={this.state.dataSource} pagination={false} />
+        </Card>), key: activeKey });
+        this.setState({ panes, activeKey });
+    }
+
+    remove = (targetKey) => {
+        let activeKey = this.state.activeKey;
+        let lastIndex;
+        this.state.panes.forEach((pane, i) => {
+            if (pane.key === targetKey) {
+                lastIndex = i - 1;
+            }
+        });
+        const panes = this.state.panes.filter(pane => pane.key !== targetKey);
+        if (lastIndex >= 0 && activeKey === targetKey) {
+            activeKey = panes[lastIndex].key;
+        }
+        this.setState({ panes, activeKey });
+    }
+
+
+
+    onCheckedKeyChange = (checkedArrs)=>{
+        console.log(checkedArrs);
+    }
+
+    render() {
+
+        const treeDatas = [{
+            title: '0-0',
+            key: '0-0',
+            children: [{
+                title: '0-0-0',
+                key: '0-0-0',
+                children: [
+                    { title: '0-0-0-0', key: '0-0-0-0' },
+                    { title: '0-0-0-1', key: '0-0-0-1' },
+                    { title: '0-0-0-2', key: '0-0-0-2' },
+                ],
+            }, {
+                title: '0-0-1',
+                key: '0-0-1',
+                children: [
+                    { title: '0-0-1-0', key: '0-0-1-0' },
+                    { title: '0-0-1-1', key: '0-0-1-1' },
+                    { title: '0-0-1-2', key: '0-0-1-2' },
+                ],
+            }, {
+                title: '0-0-2',
+                key: '0-0-2',
+            }],
+        }, {
+            title: '0-1',
+            key: '0-1',
+            children: [
+                { title: '0-1-0-0', key: '0-1-0-0' },
+                { title: '0-1-0-1', key: '0-1-0-1' },
+                { title: '0-1-0-2', key: '0-1-0-2' },
+            ],
+        }, {
+            title: '0-2',
+            key: '0-2',
+        }];
+        const treeData = [{
+            label: '全部',
+            value: 'all',
+            children:[{
+                label: '0-0',
+                value: '00',
+                children: [{
+                    label: '0-0-0',
+                    value: '000',
+                    children: [
+                        { label: '0-0-0-0-0-0-0-0-0-0-0', value: '0000' },
+                        { label: '0-0-0-1', value: '0001' },
+                        { label: '0-0-0-2', value: '0002' },
+                    ],
+                }, {
+                    label: '0-0-1',
+                    value: '001',
+                    children: [
+                        { label: '0-0-1-0', value: '0010' },
+                        { label: '0-0-1-1', value: '0011' },
+                        { label: '0-0-1-2', value: '0012' },
+                    ],
+                }, {
+                    label: '0-0-2',
+                    value: '002',
+                }],
+            }, {
+                label: '0-1',
+                value: '01',
+                children: [
+                    { label: '0-1-0-0', value: '0100' },
+                    { label: '0-1-0-1', value: '0101' },
+                    { label: '0-1-0-2', value: '0102' },
+                ],
+            }, {
+                label: '0-2',
+                value: '02',
+            }]
+        }];
+
         return (
             <div>
-                <Card title="多选SelectTree"  bordered={false}>
-                    <TreeCheck
-                        treeData={treeData}
-                        isShowSearch={true}
-                        spanName={'多选Select：'}
-                        getAllNodes={true}
-                        //LabelAndValue={['title','key']}
-                        //checkedKeys={['00','01']}
-                        multiple={true}
-                        onCheckedKeyChange={(checkedArrs) => this.onCheckedKeyChange(checkedArrs)}/>
+                <div style={{ marginBottom: 16 }}>
+                    <Button onClick={this.add}>ADD</Button>
+                </div>
+                <Tabs
+                    hideAdd
+                    onChange={this.onChange}
+                    activeKey={this.state.activeKey}
+                    type="editable-card"
+                    onEdit={this.onEdit}
+                    tabBarGutter={0}
+                >
+                    {this.state.panes.map(pane => <TabPane tab={pane.title} key={pane.key}>{pane.content}</TabPane>)}
+                </Tabs>
 
-                    <h3>API</h3>
-                    <p>TreeCheck 为多选SelectTree，本组件为适应特殊场景而封装。</p>
-                    <Table columns={columns} dataSource={data} pagination={false} />
-                </Card>
+                {/*<Card title="多选SelectTree"  bordered={false}>*/}
+                    {/*<TreeCheck*/}
+                        {/*treeData={treeData}*/}
+                        {/*isShowSearch={true}*/}
+                        {/*spanName={'多选Select：'}*/}
+                        {/*getAllNodes={true}*/}
+                        {/*//LabelAndValue={['title','key']}*/}
+                        {/*//checkedKeys={['00','01']}*/}
+                        {/*multiple={true}*/}
+                        {/*onCheckedKeyChange={(checkedArrs) => this.onCheckedKeyChange(checkedArrs)}/>*/}
+
+                    {/*<h3>API</h3>*/}
+                    {/*<p>TreeCheck 为多选SelectTree，本组件为适应特殊场景而封装。</p>*/}
+                    {/*<Table columns={this.state.columns} dataSource={this.state.dataSource} pagination={false} />*/}
+                {/*</Card>*/}
             </div>
         );
     }
