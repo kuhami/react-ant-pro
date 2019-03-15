b388caea155e301ceb96b7a1f3dfbddc4cd62036
liu.yalei <liu.yalei@mwee.cn>
2019-03-15 16:19:35 +0800

    cancel:proxy

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -8,7 +8,8 @@ import { getNotices } from './mock/notices';
 import { format, delay } from 'roadhog-api-doc';
 
 // 是否禁用代理
-const noProxy = process.env.NO_PROXY === 'true';
+// const noProxy = process.env.NO_PROXY === 'true';
+const noProxy = true;
 
 // 代码中会兼容本地 service mock 以及部署站点的静态数据
 const proxy = {
