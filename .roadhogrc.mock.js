ddd78cd4696554dda95424fef24397ce3c067875
liu.yalei <liu.yalei@mwee.cn>
2019-03-19 14:11:22 +0800

    noProxy:true

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -7,7 +7,7 @@ import { getProfileAdvancedData } from './mock/profile';
 import { getNotices } from './mock/notices';
 import { format, delay } from 'roadhog-api-doc';
 
-// 是否禁用代理
+// 是否禁用代理noProxy:true 不使用代理
 const noProxy = process.env.NO_PROXY === 'true';
 
 // 代码中会兼容本地 service mock 以及部署站点的静态数据
