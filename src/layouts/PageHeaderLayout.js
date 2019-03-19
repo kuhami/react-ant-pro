f6f32306682141f7ec91be2dc8d32d09fbc4f748
liu.yalei <liu.yalei@mwee.cn>
2019-03-19 15:17:28 +0800

    change:PageHeader

--- src/layouts/PageHeaderLayout.js
+++ src/layouts/PageHeaderLayout.js
@@ -4,7 +4,7 @@ import PageHeader from '../components/PageHeader';
 import styles from './PageHeaderLayout.less';
 
 export default ({ children, wrapperClassName, top, ...restProps }) => (
-  <div style={{ margin: '-24px -24px 0' }} className={wrapperClassName}>
+  <div style={{ margin: '-14px -24px 0' }} className={wrapperClassName}>
     {top}
     <PageHeader key="pageheader" {...restProps} linkElement={Link} />
     {children ? <div className={styles.content}>{children}</div> : null}
